0100056be4ad76ca40a1fd102db845fa98c6b66d
JennyVall0606 <jaov669@gmail.com>
2025-02-13 19:57:43 -0500

    EstructuraAngularColomboBest

---
+++ src/app/domains/seller-contact/seller-contact.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-contact',
+  imports: [],
+  templateUrl: './seller-contact.component.html',
+  styleUrl: './seller-contact.component.css'
+})
+export class SellerContactComponent {
+
+}
---
+++ src/app/domains/tour-info/tour-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tour-info',
+  imports: [],
+  templateUrl: './tour-info.component.html',
+  styleUrl: './tour-info.component.css'
+})
+export class TourInfoComponent {
+
+}
