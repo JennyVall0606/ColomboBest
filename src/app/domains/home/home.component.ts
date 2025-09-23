1d669946055641d44d36c0f4455634ebd4c548ed
Jenny Alexandra Oliva Vallejos <jennyvallejos@MacBook-Pro-de-Jenny.local>
2025-09-22 19:25:43 -0500

    home completo

--- src/app/domains/home/home.component.ts
+++ src/app/domains/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],  // 👈 Importa NgFor y otras directivas comunes
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  destinos = [
+    { nombre: 'Caño Cristales', ubicacion: 'Meta, Col.', imagen: 'assets/cano-cristales.jpg' },
+    { nombre: 'Bahía Solano', ubicacion: 'Chocó, Col.', imagen: 'assets/bahia-solano.jpg' },
+    { nombre: 'Nuquí', ubicacion: 'Chocó, Col.', imagen: 'assets/nuqui.jpg' }
+  ];
 }
